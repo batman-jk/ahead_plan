07413bf07009cbdbd55f1289c05d88c3af489482
Jaya Krishna <iambatman6911@gmail.com>
2026-03-04 15:19:53 +0530

    fixed ahead structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import AheadPlan from './ahead-b2c-deepdive'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AheadPlan />
   </StrictMode>,
 )
